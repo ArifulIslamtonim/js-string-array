e570c62b1b0d65956f602ac3ff07aaf43b15d47f
AriulIslamtonim <arifulislamtonim@gmail.com>
2022-02-15 23:19:15 +0600

    array slice

---
+++ array-slice.js
@@ -0,0 +1,13 @@
+const numbers  = [3, 5, 6, 12, 22, 24, 33, 44];
+// const numbersSliced = numbers.slice(4, 7);
+// console.log(numbersSliced);
+// console.log(numbers);
+
+// const numbersSpliced = numbers.splice(4, 2);
+// numbers.shift();
+// console.log(numbersSpliced);
+// console.log(numbers);
+
+const numbersSpliced2 = numbers.splice(3,3,333, 444, 990, 55, 666, 77);
+console.log(numbersSpliced2);
+console.log(numbers);
