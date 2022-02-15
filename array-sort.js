526652fdd60ec3fdf67eef496d3241678ebcf4f1
AriulIslamtonim <arifulislamtonim@gmail.com>
2022-02-16 00:41:08 +0600

    array sort

---
+++ array-sort.js
@@ -0,0 +1,11 @@
+const numbers = [3, 9, 5, 77, 88, 123, 432, 546, 1, 2, 7, 6, 8, 4];
+const sortNumbers = numbers.sort(function(x,y){
+    return y-x;
+});
+// console.log(sortNumbers);
+
+const friends = ['abu', 'sabu', 'labu', 'babu', 'cantu', 'danti', 'caby'];
+const sortFriends = friends.sort();
+console.log(sortFriends);
+// const reverseFriends = friends.reverse();
+// console.log(reverseFriends);
